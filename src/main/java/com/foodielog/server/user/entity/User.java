1c0f98c4bf72d7ad6e4eb8ae84509be1ab4a5d86
chaewon <djacltk@naver.com>
2023-08-09 16:35:48 +0900

    fix: User entity 컬럼 조건 추가
    1. UserStatus Enum 맵핑
    2. email, nickName unique 조건

--- src/main/java/com/foodielog/server/user/entity/User.java
+++ src/main/java/com/foodielog/server/user/entity/User.java
@@ -31,49 +31,50 @@ import lombok.NoArgsConstructor;
 @Table(name = "user_tb")
 @Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, length = 255)
-    private String email;
+	@Column(nullable = false, length = 255, unique = true)
+	private String email;
 
-    @Column(nullable = false, length = 60)
-    private String password;
+	@Column(nullable = false, length = 60)
+	private String password;
 
-    @Column(nullable = false, length = 5)
-    @Enumerated(EnumType.STRING)
-    private ProviderType provider;
+	@Column(nullable = false, length = 5)
+	@Enumerated(EnumType.STRING)
+	private ProviderType provider;
 
-    @Column(nullable = false, length = 10)
-    @Enumerated(EnumType.STRING)
-    private Role role;
+	@Column(nullable = false, length = 10)
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    @Column(nullable = false, length = 50)
-    private String nickName;
+	@Column(nullable = false, length = 50, unique = true)
+	private String nickName;
 
-    @Column(length = 255)
-    private String profileImageUrl;
+	@Column(length = 255)
+	private String profileImageUrl;
 
-    @Column(length = 150)
-    private String aboutMe;
+	@Column(length = 150)
+	private String aboutMe;
 
-    @Column(nullable = false, length = 1)
-    @Enumerated(EnumType.STRING)
-    @ColumnDefault("'Y'")
-    private Flag notificationFlag;
+	@Column(nullable = false, length = 1)
+	@Enumerated(EnumType.STRING)
+	@ColumnDefault("'Y'")
+	private Flag notificationFlag;
 
-    @Column(nullable = false, length = 1)
-    @Enumerated(EnumType.STRING)
-    @ColumnDefault("'N'")
-    private Flag badgeFlag;
+	@Column(nullable = false, length = 1)
+	@Enumerated(EnumType.STRING)
+	@ColumnDefault("'N'")
+	private Flag badgeFlag;
 
-    @Column(nullable = false, length = 5)
-    private UserStatus status;
-    
-    @CreationTimestamp
-    private Timestamp createdAt;
+	@Column(nullable = false, length = 10)
+	@Enumerated(EnumType.STRING)
+	private UserStatus status;
 
-    @UpdateTimestamp
-    private Timestamp updatedAt;
+	@CreationTimestamp
+	private Timestamp createdAt;
+
+	@UpdateTimestamp
+	private Timestamp updatedAt;
 }
