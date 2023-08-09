a5b5ef23a086db1683cb71f032617a0c7450eb04
Eom Chae Won <djacltk@naver.com>
2023-08-09 19:58:45 +0900

    [Refactor] #11 전체 패키지 구조 수정 (#12)
    * rename: types 패키지 정리
    
    각 관련 도메인 패키지 내부로 이동
    
    * rename: 패키지 구조 전체 수정
    
    * rename: entity 패키지 추가
    
    * rename: 패키지 구조 변경

---
+++ src/main/java/com/foodielog/server/feed/entity/Feed.java
@@ -0,0 +1,62 @@
+package com.foodielog.server.feed.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.foodielog.server.feed.type.ContentStatus;
+import com.foodielog.server.restaurant.entity.Restaurant;
+import com.foodielog.server.user.entity.User;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@DynamicInsert
+@Table(name = "feed_tb")
+@Entity
+public class Feed {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "restaurant_id")
+	private Restaurant restaurant;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(nullable = false, length = 255)
+	private String thumbnailUrl;
+
+	@Column(nullable = false, length = 2000)
+	private String content;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 7)
+	private ContentStatus status;
+
+	@CreationTimestamp
+	private Timestamp createdAt;
+
+	@UpdateTimestamp
+	private Timestamp updatedAt;
+}
---
+++ src/main/java/com/foodielog/server/user/type/Flag.java
@@ -0,0 +1,6 @@
+package com.foodielog.server.user.type;
+
+public enum Flag {
+	YES,
+	NO;
+}
