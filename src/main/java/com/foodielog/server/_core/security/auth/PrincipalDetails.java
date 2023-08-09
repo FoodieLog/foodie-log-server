ad755871696e7c8dd65144856ca10e458b120c12
chaewon <djacltk@naver.com>
2023-08-09 20:44:26 +0900

    fix: rebase conflict

--- src/main/java/com/foodielog/server/_core/security/auth/PrincipalDetails.java
+++ src/main/java/com/foodielog/server/_core/security/auth/PrincipalDetails.java
@@ -7,8 +7,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.foodielog.server.types.UserStatus;
 import com.foodielog.server.user.entity.User;
+import com.foodielog.server.user.type.UserStatus;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
