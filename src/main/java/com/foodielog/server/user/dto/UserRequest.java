93aad823f5c6b4b848850b1fe0c05e1d0973605c
chaewon <djacltk@naver.com>
2023-08-10 16:32:26 +0900

    feat: jwt 로그인 로직 (#9)

---
+++ src/main/java/com/foodielog/server/user/dto/UserRequest.java
@@ -0,0 +1,21 @@
+package com.foodielog.server.user.dto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+
+public class UserRequest {
+
+	@Getter
+	public static class LoginDTO {
+		@Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "이메일 형식이 올바르지 않습니다.")
+		@NotEmpty
+		private String email;
+
+		@Size(max = 60)
+		@NotEmpty
+		private String password;
+	}
+}
