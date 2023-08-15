78a0e21b6a80757e85531a2896a97dff46a64936
inyoung <charmingyear@gmail.com>
2023-08-16 00:36:17 +0900

    refactor: follow, feedLike 엔티티의 notification 필드 삭제 (#32)

--- core/src/main/java/com/foodielog/server/feed/entity/Media.java
+++ core/src/main/java/com/foodielog/server/feed/entity/Media.java
@@ -1,45 +1,36 @@
 package com.foodielog.server.feed.entity;
 
-import java.sql.Timestamp;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "media_tb")
 @Entity
 public class Media {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "feed_id")
-	private Feed feed;
+    @ManyToOne
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
 
-	@Column(name = "image_url", length = 80)
-	private String imageUrl;
+    @Column(name = "image_url", length = 255)
+    private String imageUrl;
 
-	@CreationTimestamp
-	private Timestamp createdAt;
+    @CreationTimestamp
+    private Timestamp createdAt;
 
-	public static Media createMedia(Feed feed, String imageUrl) {
-		Media media = new Media();
-		media.feed = feed;
-		media.imageUrl = imageUrl;
-		return media;
-	}
+    public static Media createMedia(Feed feed, String imageUrl) {
+        Media media = new Media();
+        media.feed = feed;
+        media.imageUrl = imageUrl;
+        return media;
+    }
 }
