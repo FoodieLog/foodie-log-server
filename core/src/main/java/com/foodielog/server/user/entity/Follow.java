9e07cabec9376551953019269ccd099454ca1f0b
inyoung <charmingyear@gmail.com>
2023-08-16 00:07:08 +0900

    refactor: follow entity 복합키 설정, 어노테이션 추가 (#32)

--- core/src/main/java/com/foodielog/server/user/entity/Follow.java
+++ core/src/main/java/com/foodielog/server/user/entity/Follow.java
@@ -1,29 +1,35 @@
 package com.foodielog.server.user.entity;
 
-import java.sql.Timestamp;
-
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
+import com.foodielog.server.notification.entity.Notification;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
-import com.foodielog.server.notification.entity.Notification;
+import javax.persistence.*;
+import java.sql.Timestamp;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "follow_tb")
+@Entity
+@IdClass(FollowPK.class)
 public class Follow {
-	@Id
-	@Column(name = "following_id")
-	private User followingId; // 나
 
-	@Id
-	@Column(name = "followed_id")
-	private User followedId; // 너
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id")
+    private User followingId; // 나
 
-	@ManyToOne
-	@JoinColumn(name = "notification")
-	private Notification notification;
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "followed_id")
+    private User followedId; // 너
 
-	@CreationTimestamp
-	private Timestamp createdAt; // 나 -> 너
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "notification")
+    private Notification notification;
+
+    @CreationTimestamp
+    private Timestamp createdAt; // 나 -> 너
 }
---
+++ core/src/main/java/com/foodielog/server/user/entity/FollowPK.java
@@ -0,0 +1,8 @@
+package com.foodielog.server.user.entity;
+
+import java.io.Serializable;
+
+public class FollowPK implements Serializable {
+    Long followingId;
+    Long followedId;
+}
