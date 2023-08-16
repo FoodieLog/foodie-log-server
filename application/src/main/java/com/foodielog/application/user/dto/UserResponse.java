97a20fc514b5a86b631aa6ad1924f680aee53408
youngjun <sohn919@naver.com>
2023-08-16 14:20:09 +0900

    feat: 마이페이지 썸네일 리스트 기능 추가 (#29)

--- application/src/main/java/com/foodielog/application/user/dto/UserResponse.java
+++ application/src/main/java/com/foodielog/application/user/dto/UserResponse.java
@@ -1,9 +1,12 @@
 package com.foodielog.application.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.foodielog.server.feed.entity.Feed;
 import com.foodielog.server.user.entity.User;
 
 import lombok.Getter;
+
+import java.util.List;
 
 public class UserResponse {
 	@Getter
@@ -20,6 +23,45 @@ public class UserResponse {
 			this.profileImageUrl = user.getProfileImageUrl();
 			this.accessToken = accessToken;
 			this.refreshToken = refreshToken;
+		}
+	}
+
+	@Getter
+	public static class ProfileDTO {
+		private final Long userId;
+		private final String profileImageUrl;
+		private final String aboutMe;
+		private final Long feedCount;
+		private final Long follower;
+		private final Long following;
+
+		public ProfileDTO(User user, Long feedCount, Long follower, Long following) {
+			this.userId = user.getId();
+			this.profileImageUrl = user.getProfileImageUrl();
+			this.aboutMe = user.getAboutMe();
+			this.feedCount = feedCount;
+			this.follower = follower;
+			this.following = following;
+		}
+	}
+
+	@Getter
+	public static class ThumbnailListDTO {
+		private final List<ThumbnailDTO> content;
+
+		public ThumbnailListDTO(List<ThumbnailDTO> content) {
+			this.content = content;
+		}
+	}
+
+	@Getter
+	public static class ThumbnailDTO {
+		private final Long id;
+		private final String thumbnailUrl;
+
+		public ThumbnailDTO(Feed feed) {
+			this.id = feed.getId();
+			this.thumbnailUrl = feed.getThumbnailUrl();
 		}
 	}
 }
--- core/src/main/java/com/foodielog/server/feed/repository/FeedRepository.java
+++ core/src/main/java/com/foodielog/server/feed/repository/FeedRepository.java
@@ -1,7 +1,18 @@
 package com.foodielog.server.feed.repository;
 
 import com.foodielog.server.feed.entity.Feed;
+import com.foodielog.server.user.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    Long countByUser(User user);
+
+    @Query("SELECT f FROM Feed f " +
+            "WHERE f.user = :user AND f.id > :feedId")
+    List<Feed> getThumbnail(@Param("user") User user, @Param("feedId") Long feedId, Pageable pageable);
 }
