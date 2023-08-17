2b7d65912efc1d024828266cc5023ed4408ceef1
inyoung <charmingyear@gmail.com>
2023-08-17 22:00:58 +0900

    refactor: s3 uploader 예외처리, 중복체크 추가 구현 (#39)

--- core/src/main/java/com/foodielog/server/_core/s3/S3Uploader.java
+++ core/src/main/java/com/foodielog/server/_core/s3/S3Uploader.java
@@ -1,7 +1,11 @@
 package com.foodielog.server._core.s3;
 
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.foodielog.server._core.error.ErrorMessage;
+import com.foodielog.server._core.error.exception.Exception400;
 import com.foodielog.server._core.error.exception.Exception500;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,27 +22,126 @@ import java.io.IOException;
 public class S3Uploader {
 
     private final AmazonS3 amazonS3;
+    private Set<String> uploadedFileNames = new HashSet<>();
+    private Set<Long> uploadedFileSizes = new HashSet<>();
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String saveFile(MultipartFile multipartFile) {
-        String originalFilename = multipartFile.getOriginalFilename();
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxSizeString;
 
-        log.info("File upload started: " + originalFilename);
+    public List<String> saveFiles(List<MultipartFile> multipartFiles) {
+        List<String> uploadedUrls = new ArrayList<>();
+        long maxSize = parseMaxSize(maxSizeString);
+
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (multipartFile.getSize() > maxSize) {
+                throw new Exception400("file : ", ErrorMessage.EXCEED_IMAGE_SIZE);
+            }
+
+            String uploadedUrl = saveFile(multipartFile);
+            uploadedUrls.add(uploadedUrl);
+        }
+
+        clear();
+        return uploadedUrls;
+    }
+
+    public void deleteFile(String fileUrl) {
+        String[] urlParts = fileUrl.split("/");
+        String fileBucket = urlParts[2].replace(".s3.amazonaws.com", "");
+
+        if (!fileBucket.equals(bucket)) {
+            throw new Exception400("fileUrl", ErrorMessage.NO_IMAGE_EXIST);
+        }
+
+        String objectKey = String.join("/", Arrays.copyOfRange(urlParts, 3, urlParts.length));
+
+        if (!amazonS3.doesObjectExist(bucket, objectKey)) {
+            throw new Exception400("fileUrl", ErrorMessage.NO_IMAGE_EXIST);
+        }
+
+        try {
+            amazonS3.deleteObject(bucket, objectKey);
+        } catch (AmazonS3Exception e) {
+            log.error("File delete fail : " + e.getMessage());
+            throw new Exception500(ErrorMessage.FAIL_DELETE);
+        } catch (SdkClientException e) {
+            log.error("AWS SDK client error : " + e.getMessage());
+            throw new Exception500(ErrorMessage.FAIL_DELETE);
+        }
+    }
+
+    private long parseMaxSize(String maxSizeString) {
+        String numericValue = maxSizeString.replaceAll("[^0-9]", "");
+        return Long.parseLong(numericValue) * 1024 * 1024;
+    }
+
+    private String saveFile(MultipartFile multipartFile) {
+        if (isDuplicate(multipartFile)) {
+            throw new Exception400("file", ErrorMessage.DUPLICATE_IMAGE);
+        }
+
+        String randomFilename = generateRandomFilename(multipartFile);
+
+        log.info("File upload started: " + randomFilename);
 
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(multipartFile.getSize());
         metadata.setContentType(multipartFile.getContentType());
 
         try {
-            amazonS3.putObject(bucket, originalFilename, multipartFile.getInputStream(), metadata);
+            amazonS3.putObject(bucket, randomFilename, multipartFile.getInputStream(), metadata);
+        } catch (AmazonS3Exception e) {
+            log.error("Amazon S3 error while uploading file: " + e.getMessage());
+            throw new Exception500(ErrorMessage.FAIL_UPLOAD);
+        } catch (SdkClientException e) {
+            log.error("AWS SDK client error while uploading file: " + e.getMessage());
+            throw new Exception500(ErrorMessage.FAIL_UPLOAD);
         } catch (IOException e) {
-            throw new Exception500("서버 오류 #F");
+            log.error("IO error while uploading file: " + e.getMessage());
+            throw new Exception500(ErrorMessage.FAIL_UPLOAD);
         }
 
-        log.info("File upload completed: " + originalFilename);
+        log.info("File upload completed: " + randomFilename);
 
-        return amazonS3.getUrl(bucket, originalFilename).toString();
+        return amazonS3.getUrl(bucket, randomFilename).toString();
+    }
+
+    private boolean isDuplicate(MultipartFile multipartFile) {
+        String fileName = multipartFile.getOriginalFilename();
+        Long fileSize = multipartFile.getSize();
+
+        if (uploadedFileNames.contains(fileName) && uploadedFileSizes.contains(fileSize)) {
+            return true;
+        }
+
+        uploadedFileNames.add(fileName);
+        uploadedFileSizes.add(fileSize);
+
+        return false;
+    }
+
+    private void clear() {
+        uploadedFileNames.clear();
+        uploadedFileSizes.clear();
+    }
+
+    private String generateRandomFilename(MultipartFile multipartFile) {
+        String originalFilename = multipartFile.getOriginalFilename();
+        String fileExtension = validateFileExtension(originalFilename);
+        String randomFilename = UUID.randomUUID() + "." + fileExtension;
+        return randomFilename;
+    }
+
+    private String validateFileExtension(String originalFilename) {
+        String fileExtension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
+        List<String> allowedExtensions = Arrays.asList("jpg", "png", "gif", "jpeg");
+
+        if (!allowedExtensions.contains(fileExtension)) {
+            throw new Exception400("file", ErrorMessage.NOT_IMAGE_EXTENSION);
+        }
+        return fileExtension;
     }
 }
