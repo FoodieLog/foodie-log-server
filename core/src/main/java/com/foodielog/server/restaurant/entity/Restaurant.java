e38a5ce445b0a029cedf69b6e0d89259c091f92e
inyoung <charmingyear@gmail.com>
2023-08-18 16:18:45 +0900

    feat: kakao open api의 place_id 필드 추가 (#47)

--- core/src/main/java/com/foodielog/server/restaurant/entity/Restaurant.java
+++ core/src/main/java/com/foodielog/server/restaurant/entity/Restaurant.java
@@ -1,60 +1,59 @@
 package com.foodielog.server.restaurant.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "restaurant_tb")
 @Entity
 public class Restaurant {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false)
-	private String name;
+    @Column(nullable = false)
+    private String kakaoPlaceId;
 
-	@Column(nullable = false)
-	private String phone;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false)
-	private String category;
+    @Column(nullable = false)
+    private String phone;
 
-	@Column(nullable = false)
-	private String link;
+    @Column(nullable = false)
+    private String category;
 
-	@Column(nullable = false)
-	private String mapX;
+    @Column(nullable = false)
+    private String link;
 
-	@Column(nullable = false)
-	private String mapY;
+    @Column(nullable = false)
+    private String mapX;
 
-	@Column(nullable = false)
-	private String address;
+    @Column(nullable = false)
+    private String mapY;
 
-	@Column(nullable = false)
-	private String roadAddress;
+    @Column(nullable = false)
+    private String address;
 
-	public static Restaurant createRestaurant(String name, String phone, String category, String link, String mapX, String mapY
-			, String address, String roadAddress) {
-		Restaurant restaurant = new Restaurant();
-		restaurant.name = name;
-		restaurant.phone = phone;
-		restaurant.category = category;
-		restaurant.link = link;
-		restaurant.mapX = mapX;
-		restaurant.mapY = mapY;
-		restaurant.address = address;
-		restaurant.roadAddress = roadAddress;
-		return restaurant;
-	}
+    @Column(nullable = false)
+    private String roadAddress;
+
+    public static Restaurant createRestaurant(String name, String kakaoPlaceId, String phone, String category
+            , String link, String mapX, String mapY, String address, String roadAddress) {
+        Restaurant restaurant = new Restaurant();
+        restaurant.name = name;
+        restaurant.kakaoPlaceId = kakaoPlaceId;
+        restaurant.phone = phone;
+        restaurant.category = category;
+        restaurant.link = link;
+        restaurant.mapX = mapX;
+        restaurant.mapY = mapY;
+        restaurant.address = address;
+        restaurant.roadAddress = roadAddress;
+        return restaurant;
+    }
 }
