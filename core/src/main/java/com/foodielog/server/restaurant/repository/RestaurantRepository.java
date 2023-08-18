73b203840a660a0928ca39c280b38db7d4603d81
inyoung <charmingyear@gmail.com>
2023-08-18 16:20:43 +0900

    feat: 맛집 저장 시 중복 체크 기능 구현 (#47)

--- core/src/main/java/com/foodielog/server/restaurant/repository/RestaurantRepository.java
+++ core/src/main/java/com/foodielog/server/restaurant/repository/RestaurantRepository.java
@@ -3,5 +3,8 @@ package com.foodielog.server.restaurant.repository;
 import com.foodielog.server.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    Optional<Restaurant> findByKakaoPlaceId(String kakaoPlaceId);
 }
