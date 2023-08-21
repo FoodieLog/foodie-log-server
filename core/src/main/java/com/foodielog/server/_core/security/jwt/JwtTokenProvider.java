2de626b7bde12c05c7f3f6c0d0565210509f6055
chaewon <djacltk@naver.com>
2023-08-21 21:37:57 +0900

    fix: Refresh Token은 회원 정보(권한)을 가지지 않는다 (#64)
    Refresh Token의 역할은 Access Token 재발급이다.
    때문에 접근에 대한 권한을 주면 안된다.

--- core/src/main/java/com/foodielog/server/_core/security/jwt/JwtTokenProvider.java
+++ core/src/main/java/com/foodielog/server/_core/security/jwt/JwtTokenProvider.java
@@ -1,112 +1,102 @@
 package com.foodielog.server._core.security.jwt;
 
-import java.security.Key;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.foodielog.server._core.error.exception.Exception400;
+import com.foodielog.server._core.security.auth.PrincipalDetails;
+import com.foodielog.server._core.security.auth.PrincipalDetailsService;
+import com.foodielog.server.user.entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import com.foodielog.server._core.error.exception.Exception400;
-import com.foodielog.server._core.security.auth.PrincipalDetails;
-import com.foodielog.server._core.security.auth.PrincipalDetailsService;
-import com.foodielog.server.user.entity.User;
-
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SignatureException;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
-	public static final Long EXP_ACCESS = 1000L * 60 * 60 * 24; // 24시간
-	public static final Long EXP_REFRESH = 1000L * 60 * 60 * 24 * 30; // 30일
-	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String HEADER = "Authorization";
+    public static final Long EXP_ACCESS = 1000L * 60 * 60 * 24; // 24시간
+    public static final Long EXP_REFRESH = 1000L * 60 * 60 * 24 * 30; // 30일
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER = "Authorization";
 
-	private final PrincipalDetailsService principalDetailsService;
+    private final PrincipalDetailsService principalDetailsService;
 
-	private Key JWT_KEY;
+    private Key JWT_KEY;
 
-	@Autowired
-	public JwtTokenProvider(@Value("${jwt.key}") String secretKey, PrincipalDetailsService principalDetailsService) {
-		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-		JWT_KEY = Keys.hmacShaKeyFor(keyBytes);
+    @Autowired
+    public JwtTokenProvider(@Value("${jwt.key}") String secretKey, PrincipalDetailsService principalDetailsService) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        JWT_KEY = Keys.hmacShaKeyFor(keyBytes);
 
-		this.principalDetailsService = principalDetailsService;
-	}
+        this.principalDetailsService = principalDetailsService;
+    }
 
-	public String createAccessToken(User user) {
-		String jwt = Jwts.builder()
-			.setSubject(user.getEmail())
-			.setExpiration(new Date(System.currentTimeMillis() + EXP_ACCESS))
-			.claim("id", user.getId())
-			.claim("role", String.valueOf(user.getRole()))
-			.signWith(JWT_KEY, SignatureAlgorithm.HS256)
-			.compact();
-		return jwt;
-	}
+    public String createAccessToken(User user) {
+        String jwt = Jwts.builder()
+                .setSubject(user.getEmail())
+                .setExpiration(new Date(System.currentTimeMillis() + EXP_ACCESS))
+                .claim("id", user.getId())
+                .claim("role", String.valueOf(user.getRole()))
+                .signWith(JWT_KEY, SignatureAlgorithm.HS256)
+                .compact();
+        return jwt;
+    }
 
-	public String createRefreshToken(User user) {
-		String jwt = Jwts.builder()
-			.setSubject(user.getEmail())
-			.setExpiration(new Date(System.currentTimeMillis() + EXP_REFRESH))
-			.claim("id", user.getId())
-			.claim("role", String.valueOf(user.getRole()))
-			.signWith(JWT_KEY, SignatureAlgorithm.HS256)
-			.compact();
-		return jwt;
-	}
+    public String createRefreshToken(User user) {
+        String jwt = Jwts.builder()
+                .setExpiration(new Date(System.currentTimeMillis() + EXP_REFRESH))
+                .signWith(JWT_KEY, SignatureAlgorithm.HS256)
+                .compact();
+        return jwt;
+    }
 
-	// Request Header에서 token 값 추출
-	public String resolveToken(HttpServletRequest request) {
-		String header = request.getHeader("Authorization");
-		return header.replaceAll(TOKEN_PREFIX, "");
-	}
+    // Request Header에서 token 값 추출
+    public String resolveToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        return header.replaceAll(TOKEN_PREFIX, "");
+    }
 
-	// 토큰 검증
-	public boolean isTokenValid(String token) {
-		try {
-			Jwts.parserBuilder()
-				.setSigningKey(JWT_KEY)
-				.build()
-				.parseClaimsJws(token);
-			return true;
-		} catch (SignatureException ex) {
-			throw new Exception400("message", "Invalid JWT signature");
-		} catch (MalformedJwtException ex) {
-			throw new Exception400("message", "Invalid JWT token");
-		} catch (ExpiredJwtException ex) {
-			throw new Exception400("message", "Expired JWT token");
-		} catch (UnsupportedJwtException ex) {
-			throw new Exception400("message", "Unsupported JWT token");
-		} catch (IllegalArgumentException ex) {
-			throw new Exception400("message", "JWT claims string is empty.");
-		}
-	}
+    // 토큰 검증
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(JWT_KEY)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            throw new Exception400("message", "Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            throw new Exception400("message", "Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new Exception400("message", "Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            throw new Exception400("message", "Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            throw new Exception400("message", "JWT claims string is empty.");
+        }
+    }
 
-	// 토큰으로 Authentication 객체 생성
-	public Authentication getAuthentication(String jwt) {
-		PrincipalDetails principalDetails = (PrincipalDetails)principalDetailsService.loadUserByUsername(getEmail(jwt));
-		return new UsernamePasswordAuthenticationToken(principalDetails, "", principalDetails.getAuthorities());
-	}
+    // 토큰으로 Authentication 객체 생성
+    public Authentication getAuthentication(String jwt) {
+        PrincipalDetails principalDetails = (PrincipalDetails) principalDetailsService.loadUserByUsername(getEmail(jwt));
+        return new UsernamePasswordAuthenticationToken(principalDetails, "", principalDetails.getAuthorities());
+    }
 
-	// 토큰에서 email 정보 추출
-	private String getEmail(String jwt) {
-		return Jwts.parserBuilder()
-			.setSigningKey(JWT_KEY)
-			.build()
-			.parseClaimsJws(jwt)
-			.getBody()
-			.getSubject();
-	}
+    // 토큰에서 email 정보 추출
+    private String getEmail(String jwt) {
+        return Jwts.parserBuilder()
+                .setSigningKey(JWT_KEY)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody()
+                .getSubject();
+    }
 
 }
