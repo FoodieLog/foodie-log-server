6e81bd1eacaeee4d65013a4a25cf89af2bd2f6b8
inyoung <charmingyear@gmail.com>
2023-08-22 16:23:12 +0900

    refactor: 필요 없는 어노테이션 제거 (#66)

--- application/src/main/java/com/foodielog/application/restaurant/dto/LikeRestaurantDTO.java
+++ application/src/main/java/com/foodielog/application/restaurant/dto/LikeRestaurantDTO.java
@@ -2,14 +2,10 @@ package com.foodielog.application.restaurant.dto;
 
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-
 public class LikeRestaurantDTO {
 
     @Getter
     public static class Request {
-
-        @NotBlank
         private Long restaurantId;
     }
 }
--- application/src/main/java/com/foodielog/application/restaurant/dto/UnlikeRestaurantDTO.java
+++ application/src/main/java/com/foodielog/application/restaurant/dto/UnlikeRestaurantDTO.java
@@ -2,14 +2,10 @@ package com.foodielog.application.restaurant.dto;
 
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
-
 public class UnlikeRestaurantDTO {
 
     @Getter
     public static class Request {
-
-        @NotBlank
         private Long restaurantId;
     }
 }
