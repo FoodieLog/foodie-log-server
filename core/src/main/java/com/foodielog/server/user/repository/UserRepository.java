c4842e2e81937b8c8234c88dccf6f58484068644
chaewon <djacltk@naver.com>
2023-08-23 16:43:11 +0900

    feat: 유저 검색 구현 (#81)

--- core/src/main/java/com/foodielog/server/user/repository/UserRepository.java
+++ core/src/main/java/com/foodielog/server/user/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.foodielog.server.user.repository;
 import com.foodielog.server.user.entity.User;
 import com.foodielog.server.user.type.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +15,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 
     Boolean existsByNickName(String nickName);
+
+    @Query("SELECT u FROM User u " +
+            "LEFT JOIN Follow f ON u = f.followedId " +
+            "WHERE u.nickName LIKE %:keyword% AND u.status = 'NORMAL'" +
+            "GROUP BY u " +
+            "ORDER BY COUNT(f.followingId) DESC")
+    List<User> searchUserOrderByFollowedIdDesc(@Param("keyword") String keyword);
 }
