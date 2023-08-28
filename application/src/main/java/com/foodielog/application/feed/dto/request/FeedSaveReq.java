f5205af756b0b940e5db978fb857bc008a383f19
inyoung <charmingyear@gmail.com>
2023-08-28 22:42:17 +0900

    fix: valid 수정 (#103)

--- application/src/main/java/com/foodielog/application/feed/dto/request/FeedSaveReq.java
+++ application/src/main/java/com/foodielog/application/feed/dto/request/FeedSaveReq.java
@@ -3,13 +3,13 @@ package com.foodielog.application.feed.dto.request;
 import com.foodielog.server._core.kakaoApi.KakaoApiResponse;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 public class FeedSaveReq {
     private KakaoApiResponse.SearchPlace selectedSearchPlace;
     private String content;
 
-    @NotBlank
+    @NotNull
     private Boolean isLiked;
 }
