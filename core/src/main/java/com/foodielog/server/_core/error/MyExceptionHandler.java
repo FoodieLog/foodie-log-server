ca6b13d46e5e7ac3f739ca3b377ed6199382f56b
chaewon <djacltk@naver.com>
2023-08-30 19:55:39 +0900

    refactor: ExceptionHandler에 로그 출력 추가 (#96)

--- core/src/main/java/com/foodielog/server/_core/error/MyExceptionHandler.java
+++ core/src/main/java/com/foodielog/server/_core/error/MyExceptionHandler.java
@@ -19,43 +19,51 @@ public class MyExceptionHandler {
 
     @ExceptionHandler(Exception400.class)
     public ResponseEntity<?> badRequest(Exception400 e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception401.class)
     public ResponseEntity<?> unAuthorized(Exception401 e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception403.class)
     public ResponseEntity<?> forbidden(Exception403 e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception404.class)
     public ResponseEntity<?> notFound(Exception404 e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     @ExceptionHandler(Exception500.class)
     public ResponseEntity<?> serverError(Exception500 e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(e.body(), e.status());
     }
 
     // @Validated 예외 처리
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<?> inValidParam(ConstraintViolationException e) {
+        log.error(e.getMessage());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(), HttpStatus.BAD_REQUEST);
         return new ResponseEntity<>(apiResult, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<?> inValid(ValidationException e) {
+        log.error(e.getMessage());
         return new ResponseEntity<>(e.body(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> unknownServerError(Exception e) {
+        log.error(e.getMessage());
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
     }
