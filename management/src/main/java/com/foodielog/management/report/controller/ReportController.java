b89f2c6b430a217e46f9a7f6f7941cf40fbf4f1d
chaewon <djacltk@naver.com>
2023-09-04 18:20:40 +0900

    feat: 신고 내역 검색 (#129)
    입력받은 파람을 조건으로 필터링하여 그 결과를
    content_id가 같은 것끼리 연속으로 나오고(:그룹화),
    그룹의 첫 데이터의 created_at을 기준으로 정렬하도록 쿼리 작성함

--- management/src/main/java/com/foodielog/management/report/controller/ReportController.java
+++ management/src/main/java/com/foodielog/management/report/controller/ReportController.java
@@ -1,15 +1,19 @@
 package com.foodielog.management.report.controller;
 
 import com.foodielog.management.report.dto.request.ProcessReq;
+import com.foodielog.management.report.dto.response.ReportListResp;
 import com.foodielog.management.report.service.ReportService;
+import com.foodielog.server._core.customValid.valid.ValidEnum;
 import com.foodielog.server._core.util.ApiUtils;
+import com.foodielog.server.feed.type.ContentStatus;
+import com.foodielog.server.report.type.ReportType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/admin/report")
@@ -23,5 +27,16 @@ public class ReportController {
     ) {
         reportService.process(request);
         return new ResponseEntity<>(ApiUtils.success(null, HttpStatus.OK), HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<ApiUtils.ApiResult<ReportListResp>> reportList(
+            @RequestParam(required = false) String nickName,
+            @RequestParam(required = false) @ValidEnum(enumClass = ReportType.class) ReportType type,
+            @RequestParam(required = false) @ValidEnum(enumClass = ContentStatus.class) ContentStatus status,
+            @PageableDefault(size = 10) Pageable pageable
+    ) {
+        ReportListResp response = reportService.getreportList(nickName, type, status, pageable);
+        return new ResponseEntity<>(ApiUtils.success(response, HttpStatus.OK), HttpStatus.OK);
     }
 }
