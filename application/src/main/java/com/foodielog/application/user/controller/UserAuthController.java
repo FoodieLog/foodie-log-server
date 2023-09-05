ef5aa147d3cc60920dcbfaf4c7dd0813c345b39f
chaewon <djacltk@naver.com>
2023-09-05 14:25:17 +0900

    feat: https healthcheck 컨트롤러 (#144)

--- application/src/main/java/com/foodielog/application/user/controller/UserAuthController.java
+++ application/src/main/java/com/foodielog/application/user/controller/UserAuthController.java
@@ -33,6 +33,11 @@ public class UserAuthController {
     private final UserAuthService userAuthService;
     private final UserOauthService oauthUserService;
 
+    @RequestMapping("/healthcheck")
+    public ResponseEntity<ApiUtils.ApiResult<Object>> healthcheck() {
+        return new ResponseEntity<>(ApiUtils.success(null, HttpStatus.OK), HttpStatus.OK);
+    }
+
     /* 토큰 재발급*/
     @GetMapping("/reissue")
     public ResponseEntity<ApiUtils.ApiResult<ReissueResp>> reissue(
