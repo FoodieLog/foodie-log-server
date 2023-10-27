5d0ce1f48610a410eb4e78f0008799f888e013ea
inyoung <charmingyear@gmail.com>
2023-10-27 18:36:44 +0900

    refactor: 맛집 중복 체크 stream 으로 리팩토링 (#213)

--- application/src/main/java/com/foodielog/application/user/service/UserService.java
+++ application/src/main/java/com/foodielog/application/user/service/UserService.java
@@ -27,7 +27,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -86,17 +88,14 @@ public class UserService {
         User feedOwner = validationUserId(userId);
         List<Feed> feeds = feedRepository.findByUserIdAndStatus(feedOwner.getId(), ContentStatus.NORMAL);
 
-        Set<Restaurant> uniqueRestaurantList = new HashSet<>();
+        List<Restaurant> RestaurantList = feeds.stream()
+                .map(Feed::getRestaurant)
+                .distinct()
+                .collect(Collectors.toList());
+
         List<UserRestaurantListResp.RestaurantListDTO> restaurantListDTOList = new ArrayList<>();
 
-        for (Feed feed : feeds) {
-            Restaurant restaurant = feed.getRestaurant();
-
-            if (uniqueRestaurantList.contains(restaurant)) {
-                continue;
-            }
-            uniqueRestaurantList.add(restaurant);
-
+        for (Restaurant restaurant : RestaurantList) {
             UserRestaurantListResp.RestaurantDTO restaurantDTO =
                     new UserRestaurantListResp.RestaurantDTO(restaurant);
 
