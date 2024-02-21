081f468b524bd4ff2782e5e33738a5592ba16da7
chaewon12 <djacltk@naver.com>
2024-02-21 16:59:20 +0900

    refactor: Management Report 도메인 ServiceDTO 분리(#225)

---
+++ management/src/main/java/com/foodielog/management/report/controller/dto/ProcessParam.java
@@ -0,0 +1,17 @@
+package com.foodielog.management.report.controller.dto;
+
+import com.foodielog.server.admin.type.ProcessedStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ProcessParam {
+
+	private Long reportedId;
+
+	private Long contentId;
+
+	private ProcessedStatus status;
+}
