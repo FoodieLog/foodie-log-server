8944097ded3da0ee15296b0606be5c8f22f0df90
chaewon12 <djacltk@naver.com>
2024-02-21 16:58:58 +0900

    refactor: Management Member 도메인 ServiceDTO 분리(#225)

---
+++ management/src/main/java/com/foodielog/management/member/controller/dto/BlockProcessedParam.java
@@ -0,0 +1,13 @@
+package com.foodielog.management.member.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class BlockProcessedParam {
+
+	private Long userId;
+
+	private String reason;
+}
