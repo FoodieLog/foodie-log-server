cdd02133bebb4345f6655ea921f7b3dfe0aa61c7
chaewon12 <djacltk@naver.com>
2024-02-21 17:07:09 +0900

    refactor: Management User 도메인 ServiceDTO 분리(#225)

---
+++ management/src/main/java/com/foodielog/management/user/controller/dto/LoginParam.java
@@ -0,0 +1,12 @@
+package com.foodielog.management.user.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class LoginParam {
+	private String email;
+
+	private String password;
+}
