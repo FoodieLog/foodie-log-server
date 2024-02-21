88a6e039b5070a8151c1656cbfe259e8aac8ca61
chaewon12 <djacltk@naver.com>
2024-02-21 17:07:09 +0900

    refactor: Management User 도메인 ServiceDTO 분리(#225)

---
+++ management/src/main/java/com/foodielog/management/user/service/dto/LoginResp.java
@@ -0,0 +1,21 @@
+package com.foodielog.management.user.service.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.foodielog.server.user.entity.User;
+
+import lombok.Getter;
+
+@Getter
+public class LoginResp {
+	private final String nickName;
+	private final String accessToken;
+
+	@JsonIgnore
+	private final String refreshToken;
+
+	public LoginResp(User user, String accessToken, String refreshToken) {
+		this.nickName = user.getNickName();
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+}
