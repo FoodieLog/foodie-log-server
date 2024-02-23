2d80da2e7e6655cf7b04f08d59e5b515a0e58ae3
chaewon12 <djacltk@naver.com>
2024-02-23 17:55:24 +0900

    refactor: UserAuthService 에서 쿼리 분리(#232)

---
+++ application/src/main/java/com/foodielog/application/user/service/UserModuleService.java
@@ -0,0 +1,52 @@
+package com.foodielog.application.user.service;
+
+import org.springframework.stereotype.Service;
+
+import com.foodielog.server._core.error.ErrorMessage;
+import com.foodielog.server._core.error.exception.Exception400;
+import com.foodielog.server.user.entity.User;
+import com.foodielog.server.user.repository.UserRepository;
+import com.foodielog.server.user.type.UserStatus;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserModuleService {
+	private final UserRepository userRepository;
+
+	public User getUser(String email) {
+		return userRepository.findByEmailAndStatus(email, UserStatus.NORMAL)
+			.orElseThrow(() -> new Exception400("email", ErrorMessage.USER_NOT_FOUND));
+	}
+
+	public Boolean isEmailExists(String email) {
+		return userRepository.existsByEmail(email);
+	}
+
+	public Boolean isNickNameExists(String nickName) {
+		return userRepository.existsByNickName(nickName);
+	}
+
+	public void checkEmailExists(String email) {
+		if (!isEmailExists(email)) {
+			throw new Exception400("email", "해당 이메일 정보가 없습니다.");
+		}
+	}
+
+	public void checkNewEmail(String email) {
+		if (isEmailExists(email)) {
+			throw new Exception400("email", "이미 가입된 이메일 입니다");
+		}
+	}
+
+	public void checkNewNickName(String nickName) {
+		if (isNickNameExists(nickName)) {
+			throw new Exception400("nickName", "이미 사용 중인 닉네임 입니다");
+		}
+	}
+
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+}
