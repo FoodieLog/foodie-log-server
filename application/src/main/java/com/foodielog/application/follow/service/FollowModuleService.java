a6bc6eceb382481583572f9fcc0d7840cfdc8133
Youngjun Son <84082544+sohn919@users.noreply.github.com>
2024-02-26 22:28:34 +0900

    refactor: notificationService 에서 쿼리 분리(#232)
    - follow, feedLike 의 moduleService 가 필요함에 따라 패키지를 추가 생성

---
+++ application/src/main/java/com/foodielog/application/follow/service/FollowModuleService.java
@@ -0,0 +1,23 @@
+package com.foodielog.application.follow.service;
+
+import com.foodielog.server.user.entity.Follow;
+import com.foodielog.server.user.entity.User;
+import com.foodielog.server.user.repository.FollowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class FollowModuleService {
+    private final FollowRepository followRepository;
+
+    public Optional<Follow> getFollowById(Long id) {
+        return followRepository.findById(id);
+    }
+
+    public boolean existsByFollowingIdAndFollowedId(User followingId, User followedId) {
+        return followRepository.existsByFollowingIdAndFollowedId(followingId, followedId);
+    }
+}
