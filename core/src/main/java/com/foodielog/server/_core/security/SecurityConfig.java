fac7f10787070432eaad3a493d522edd88ad4141
chaewon12 <djacltk@naver.com>
2024-03-20 15:54:51 +0900

    refactor: SecurityConfig 인증/인가 에러 log 정보 추가

--- core/src/main/java/com/foodielog/server/_core/security/SecurityConfig.java
+++ core/src/main/java/com/foodielog/server/_core/security/SecurityConfig.java
@@ -1,12 +1,7 @@
 package com.foodielog.server._core.security;
 
-import com.foodielog.server._core.error.exception.Exception401;
-import com.foodielog.server._core.error.exception.Exception403;
-import com.foodielog.server._core.security.jwt.JwtAuthenticationFilter;
-import com.foodielog.server._core.security.jwt.JwtExceptionFilter;
-import com.foodielog.server._core.util.FilterResponseUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,88 +16,96 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import com.foodielog.server._core.error.exception.Exception401;
+import com.foodielog.server._core.error.exception.Exception403;
+import com.foodielog.server._core.security.jwt.JwtAuthenticationFilter;
+import com.foodielog.server._core.security.jwt.JwtExceptionFilter;
+import com.foodielog.server._core.util.FilterResponseUtils;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final JwtExceptionFilter jwtExceptionFilter;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final JwtExceptionFilter jwtExceptionFilter;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // 1. CSRF 해제
-        http.csrf().disable(); // postman 접근 해야 함!! - CSR 할때!!
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		// 1. CSRF 해제
+		http.csrf().disable(); // postman 접근 해야 함!! - CSR 할때!!
 
-        // 2. iframe 거부
-        http.headers().frameOptions().sameOrigin();
+		// 2. iframe 거부
+		http.headers().frameOptions().sameOrigin();
 
-        // 3. cors 재설정
-        http.cors().configurationSource(configurationSource());
+		// 3. cors 재설정
+		http.cors().configurationSource(configurationSource());
 
-        // 4. jSessionId 사용 거부
-        // STATELESS -> 응답이 끝나기 전 까지는 세션에 저장 된다. 응답이 끝나면 삭제
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		// 4. jSessionId 사용 거부
+		// STATELESS -> 응답이 끝나기 전 까지는 세션에 저장 된다. 응답이 끝나면 삭제
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // 5. form 로그인 해제 (UsernamePasswordAuthenticationFilter 비활성화)
-        http.formLogin().disable();
+		// 5. form 로그인 해제 (UsernamePasswordAuthenticationFilter 비활성화)
+		http.formLogin().disable();
 
-        // 6. username, password 헤더 로그인 방식 해제 (BasicAuthenticationFilter 비활성화)
-        http.httpBasic().disable();
+		// 6. username, password 헤더 로그인 방식 해제 (BasicAuthenticationFilter 비활성화)
+		http.httpBasic().disable();
 
-        // 7. JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 넣는다
-        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(jwtExceptionFilter, jwtAuthenticationFilter.getClass());
+		// 7. JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 앞에 넣는다
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		http.addFilterBefore(jwtExceptionFilter, jwtAuthenticationFilter.getClass());
 
-        // 8. 인증 실패 처리
-        http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
-            log.warn("인증되지 않은 사용자가 자원에 접근하려 합니다 : " + authException.getMessage());
-            FilterResponseUtils.unAuthorized(response, new Exception401("인증되지 않았습니다"));
-        });
+		// 8. 인증 실패 처리
+		http.exceptionHandling().authenticationEntryPoint((request, response, authException) -> {
+			log.warn(request.getRequestURI() + "인증되지 않은 사용자가 자원에 접근하려 합니다 : " + authException.getMessage());
+			FilterResponseUtils.unAuthorized(response, new Exception401("인증되지 않았습니다"));
+		});
 
-        // 9. 권한 실패 처리
-        http.exceptionHandling().accessDeniedHandler((request, response, accessDeniedException) -> {
-            log.warn("권한이 없는 사용자가 자원에 접근하려 합니다 : " + accessDeniedException.getMessage());
-            FilterResponseUtils.forbidden(response, new Exception403("권한이 없습니다"));
-        });
+		// 9. 권한 실패 처리
+		http.exceptionHandling().accessDeniedHandler((request, response, accessDeniedException) -> {
+			log.warn(request.getRequestURI() + "권한이 없는 사용자가 자원에 접근하려 합니다 : " + accessDeniedException.getMessage());
+			FilterResponseUtils.forbidden(response, new Exception403("권한이 없습니다"));
+		});
 
-        // 11. 인증, 권한 필터 설정
-        http.authorizeRequests(
-                // '/api' 로 시작 하는 url 은 로그인 필요
-                authorize -> authorize.antMatchers("/api/auth/**", "/admin/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/api/feed/detail/**").permitAll()    // 누구나 접근 가능
-                        .antMatchers("/api/**").hasAnyAuthority("USER", "ADMIN")
-                        .antMatchers("/admin/**").hasAuthority("ADMIN")
-                        .anyRequest().authenticated()
-        );
+		// 11. 인증, 권한 필터 설정
+		http.authorizeRequests(
+			// '/api' 로 시작 하는 url 은 로그인 필요
+			authorize -> authorize.antMatchers("/api/auth/**", "/admin/auth/**", "/v3/api-docs/**", "/swagger-ui/**",
+					"/swagger-resources/**", "/api/feed/detail/**").permitAll()    // 누구나 접근 가능
+				.antMatchers("/api/**").hasAnyAuthority("USER", "ADMIN")
+				.antMatchers("/admin/**").hasAuthority("ADMIN")
+				.anyRequest().authenticated()
+		);
 
-        return http.build();
-    }
+		return http.build();
+	}
 
-    // cors 설정
-    public CorsConfigurationSource configurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*"); // GET, POST, PUT, DELETE (+Javascript 요청 허용)
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3001",
-                "http://192.168.0.3:3000", "https://192.168.0.3:3001", "https://172.30.1.55:3001",
-                "https://foodie-log-client.vercel.app", "https://www.foodielog.shop",
-                "https://admin-foodie-log.vercel.app"));
-        config.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
-        config.addExposedHeader("Authorization");
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config); // 모든 url에 대해서 설정을 적용한다
-        return source;
-    }
+	// cors 설정
+	public CorsConfigurationSource configurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*"); // GET, POST, PUT, DELETE (+Javascript 요청 허용)
+		config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://localhost:3001",
+			"http://192.168.0.3:3000", "https://192.168.0.3:3001", "https://172.30.1.55:3001",
+			"https://foodie-log-client.vercel.app", "https://www.foodielog.shop",
+			"https://admin-foodie-log.vercel.app"));
+		config.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+		config.addExposedHeader("Authorization");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config); // 모든 url에 대해서 설정을 적용한다
+		return source;
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws
-            Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws
+		Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 }
