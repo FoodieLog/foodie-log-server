c331d3df88c6060d1e4cdaff57a4bf334869abd3
chaewon12 <djacltk@naver.com>
2024-03-21 16:46:59 +0900

    refactor: replyModuleService 메서드 네이밍 수정

--- application/src/main/java/com/foodielog/application/notification/service/NotificationService.java
+++ application/src/main/java/com/foodielog/application/notification/service/NotificationService.java
@@ -10,17 +10,17 @@ import com.foodielog.server._core.security.jwt.JwtTokenProvider;
 import com.foodielog.server.notification.entity.Notification;
 import com.foodielog.server.notification.type.NotificationType;
 import com.foodielog.server.user.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 @RequiredArgsConstructor
 @Service
 public class NotificationService {
+
     private final NotificationModuleService notificationModuleService;
     private final ReplyModuleService replyModuleService;
     private final FeedLikeModuleService feedLikeModuleService;
@@ -54,33 +54,40 @@ public class NotificationService {
     }
 
     private void getReplyNotification(Notification notification, List<Object> notificationContent) {
-        replyModuleService.findByIdAndStatusOp(notification.getContentId())
-                .ifPresent(reply -> {
-                    NotificationListResp.ContentReply contentReply = new NotificationListResp.ContentReply(reply);
-                    NotificationListResp.ReplyNotification replyNotification =
-                            new NotificationListResp.ReplyNotification(notification, getContentUser(reply.getUser()), contentReply);
-                    notificationContent.add(replyNotification);
-                });
+        replyModuleService.getNormalOptional(notification.getContentId())
+            .ifPresent(reply -> {
+                NotificationListResp.ContentReply contentReply = new NotificationListResp.ContentReply(
+                    reply);
+                NotificationListResp.ReplyNotification replyNotification =
+                    new NotificationListResp.ReplyNotification(notification,
+                        getContentUser(reply.getUser()), contentReply);
+                notificationContent.add(replyNotification);
+            });
     }
 
     private void getLikeNotification(Notification notification, List<Object> notificationContent) {
         feedLikeModuleService.getOptionalFeedLike(notification.getContentId())
-                .ifPresent(feedLike -> {
-                    NotificationListResp.ContentFeed contentFeed = new NotificationListResp.ContentFeed(feedLike.getFeed());
-                    NotificationListResp.LikeNotification likeNotification =
-                            new NotificationListResp.LikeNotification(notification, getContentUser(feedLike.getUser()), contentFeed);
-                    notificationContent.add(likeNotification);
-                });
+            .ifPresent(feedLike -> {
+                NotificationListResp.ContentFeed contentFeed = new NotificationListResp.ContentFeed(
+                    feedLike.getFeed());
+                NotificationListResp.LikeNotification likeNotification =
+                    new NotificationListResp.LikeNotification(notification,
+                        getContentUser(feedLike.getUser()), contentFeed);
+                notificationContent.add(likeNotification);
+            });
     }
 
-    private void getFollowNotification(Notification notification, List<Object> notificationContent) {
+    private void getFollowNotification(Notification notification,
+        List<Object> notificationContent) {
         followModuleService.getOptionalFollow(notification.getContentId())
-                .ifPresent(follow -> {
-                    boolean isFollowed = followModuleService.isFollow(follow.getFollowedId(), follow.getFollowingId());
-                    NotificationListResp.FollowNotification followNotification =
-                            new NotificationListResp.FollowNotification(notification, getContentUser(follow.getFollowingId()), isFollowed);
-                    notificationContent.add(followNotification);
-                });
+            .ifPresent(follow -> {
+                boolean isFollowed = followModuleService.isFollow(follow.getFollowedId(),
+                    follow.getFollowingId());
+                NotificationListResp.FollowNotification followNotification =
+                    new NotificationListResp.FollowNotification(notification,
+                        getContentUser(follow.getFollowingId()), isFollowed);
+                notificationContent.add(followNotification);
+            });
     }
 
     private NotificationListResp.ContentUser getContentUser(User user) {
@@ -89,7 +96,8 @@ public class NotificationService {
 
     public void registerFcmToken(NotificationTokenParam parameter) {
         User user = parameter.getUser();
-        redisService.setObjectByKey(RedisService.FCM_TOKEN_PREFIX + user.getEmail(), parameter.getFcmToken(),
-                JwtTokenProvider.EXP_REFRESH, TimeUnit.MILLISECONDS);
+        redisService.setObjectByKey(RedisService.FCM_TOKEN_PREFIX + user.getEmail(),
+            parameter.getFcmToken(),
+            JwtTokenProvider.EXP_REFRESH, TimeUnit.MILLISECONDS);
     }
 }
--- application/src/main/java/com/foodielog/application/reply/service/ReplyModuleService.java
+++ application/src/main/java/com/foodielog/application/reply/service/ReplyModuleService.java
@@ -1,60 +1,58 @@
 package com.foodielog.application.reply.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.foodielog.server._core.error.exception.Exception404;
 import com.foodielog.server.feed.entity.Feed;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
 import com.foodielog.server.feed.type.ContentStatus;
 import com.foodielog.server.reply.entity.Reply;
 import com.foodielog.server.reply.repository.ReplyRepository;
 import com.foodielog.server.user.entity.User;
-
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class ReplyModuleService {
-	private final ReplyRepository replyRepository;
 
-	public Long getUserCount(User user) {
-		return replyRepository.countByUserAndStatus(user, ContentStatus.NORMAL);
-	}
+    private final ReplyRepository replyRepository;
 
-	public List<Reply> getUserReplys(User user) {
-		return replyRepository.findByUserIdAndStatus(user.getId(), ContentStatus.NORMAL);
-	}
+    public Long getUserCount(User user) {
+        return replyRepository.countByUserAndStatus(user, ContentStatus.NORMAL);
+    }
 
-	public List<Reply> getFeedReplys(Long feedId, Long lastReplyId, Pageable pageable) {
-		return replyRepository.getReplyList(feedId, lastReplyId, pageable);
-	}
+    public List<Reply> getUserReplys(User user) {
+        return replyRepository.findByUserIdAndStatus(user.getId(), ContentStatus.NORMAL);
+    }
 
-	public Reply save(Reply reply) {
-		return replyRepository.save(reply);
-	}
+    public List<Reply> getFeedReplyPage(Long feedId, Pageable pageable) {
+        return replyRepository.getReplyList(feedId, pageable);
+    }
 
-	public Reply findByIdAndUserIdAndStatus(Long replyId, Long userId) {
-		return replyRepository.findByIdAndUserIdAndStatus(replyId, userId, ContentStatus.NORMAL)
-				.orElseThrow(() -> new Exception404("해당 댓글이 없습니다."));
-	}
+    public Reply save(Reply reply) {
+        return replyRepository.save(reply);
+    }
 
-	public Reply findByIdAndStatus(Long replyId) {
-		return replyRepository.findByIdAndStatus(replyId, ContentStatus.NORMAL)
-				.orElseThrow(() -> new Exception404("해당 댓글이 없습니다."));
-	}
+    public Reply getUserReply(Long replyId, Long userId) {
+        return replyRepository.findByIdAndUserIdAndStatus(replyId, userId, ContentStatus.NORMAL)
+            .orElseThrow(() -> new Exception404("해당 댓글이 없습니다."));
+    }
 
-	public Optional<Reply> findByIdAndStatusOp(Long replyId) {
-		return replyRepository.findByIdAndStatus(replyId, ContentStatus.NORMAL);
-	}
+    public Reply getNormal(Long replyId) {
+        return replyRepository.findByIdAndStatus(replyId, ContentStatus.NORMAL)
+            .orElseThrow(() -> new Exception404("해당 댓글이 없습니다."));
+    }
 
-	public List<Reply> findByFeedIdAndStatus(Long feedId) {
-		return replyRepository.findByFeedIdAndStatus(feedId, ContentStatus.NORMAL);
-	}
+    public Optional<Reply> getNormalOptional(Long replyId) {
+        return replyRepository.findByIdAndStatus(replyId, ContentStatus.NORMAL);
+    }
 
-	public Long countByFeedAndStatus(Feed feed) {
-		return replyRepository.countByFeedAndStatus(feed, ContentStatus.NORMAL);
-	}
+    public List<Reply> getNormalReplys(Long feedId) {
+        return replyRepository.findByFeedIdAndStatus(feedId, ContentStatus.NORMAL);
+    }
+
+    public Long countReply(Feed feed) {
+        return replyRepository.countByFeedAndStatus(feed, ContentStatus.NORMAL);
+    }
 }
