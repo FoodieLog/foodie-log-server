6764b880b43b07d0b5a104c6af74cd74dc705c43
chaewon12 <djacltk@naver.com>
2024-03-21 16:35:05 +0900

    feat: 댓글 저장 req 부모id 필드 추가 (#243)

--- application/src/main/java/com/foodielog/application/reply/dto/ReplyCreateReq.java
+++ application/src/main/java/com/foodielog/application/reply/dto/ReplyCreateReq.java
@@ -1,22 +1,27 @@
 package com.foodielog.application.reply.dto;
 
 import com.foodielog.server.user.entity.User;
-import lombok.Getter;
-
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
+import lombok.Getter;
 
 @Getter
 public class ReplyCreateReq {
+
     @NotBlank(message = "내용이 공백일 수 없습니다.")
     @Size(max = 150, message = "최대 글자수는 150 입니다.")
     private String content;
 
+    @Positive
+    private Long parentId;
+
     public ReplyCreateParam toParamWith(User user, Long feedId) {
         return ReplyCreateParam.builder()
-                .user(user)
-                .feedId(feedId)
-                .content(content)
-                .build();
+            .user(user)
+            .feedId(feedId)
+            .content(content)
+            .parentId(parentId)
+            .build();
     }
 }
