b47438a8fb960f59d6607fb7d7f01986548cc1a8
chaewon12 <djacltk@naver.com>
2024-03-21 16:34:33 +0900

    feat: reply 부모-자식 구조 추가 (#243)

--- core/src/main/java/com/foodielog/server/reply/entity/Reply.java
+++ core/src/main/java/com/foodielog/server/reply/entity/Reply.java
@@ -3,20 +3,33 @@ package com.foodielog.server.reply.entity;
 import com.foodielog.server.feed.entity.Feed;
 import com.foodielog.server.feed.type.ContentStatus;
 import com.foodielog.server.user.entity.User;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
-import java.sql.Timestamp;
-
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "reply_tb")
 @Entity
 public class Reply {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -34,6 +47,13 @@ public class Reply {
 
     @Enumerated(EnumType.STRING)
     private ContentStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Reply parent;
+
+    @OneToMany(mappedBy = "parent", orphanRemoval = true)
+    private List<Reply> children = new ArrayList<>();
 
     @CreationTimestamp
     private Timestamp createdAt;
@@ -60,5 +80,9 @@ public class Reply {
 
     public void restoreReply() {
         status = ContentStatus.NORMAL;
+    }
+
+    public void updateParent(Reply parent) {
+        this.parent = parent;
     }
 }
