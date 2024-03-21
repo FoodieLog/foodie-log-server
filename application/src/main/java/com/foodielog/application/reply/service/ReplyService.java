60af5f3b6cacde281720343e2e1b4803e67a0889
chaewon12 <djacltk@naver.com>
2024-03-21 16:56:08 +0900

    feat: 댓글 저장 시 부모 id 저장 (#243)

--- application/src/main/java/com/foodielog/application/reply/service/ReplyService.java
+++ application/src/main/java/com/foodielog/application/reply/service/ReplyService.java
@@ -7,6 +7,7 @@ import com.foodielog.application.reply.dto.ReplyCreateParam;
 import com.foodielog.application.reply.dto.ReportReplyParam;
 import com.foodielog.application.reply.service.dto.ReplyCreateResp;
 import com.foodielog.application.report.service.ReportModuleService;
+import com.foodielog.server._core.error.exception.Exception400;
 import com.foodielog.server._core.error.exception.Exception404;
 import com.foodielog.server.feed.entity.Feed;
 import com.foodielog.server.notification.entity.Notification;
@@ -16,78 +17,91 @@ import com.foodielog.server.report.entity.Report;
 import com.foodielog.server.report.type.ReportType;
 import com.foodielog.server.user.entity.User;
 import com.foodielog.server.user.type.Flag;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RequiredArgsConstructor
 @Service
 public class ReplyService {
-	private final FeedModuleService feedModuleService;
-	private final ReplyModuleService replyModuleService;
-	private final NotificationModuleService notificationModuleService;
-	private final ReportModuleService reportModuleService;
 
-	private final FcmMessageProvider fcmMessageProvider;
+    private final FeedModuleService feedModuleService;
+    private final ReplyModuleService replyModuleService;
+    private final NotificationModuleService notificationModuleService;
+    private final ReportModuleService reportModuleService;
 
-	@Transactional
-	public ReplyCreateResp createReply(ReplyCreateParam parameter) {
-		Feed feed = feedModuleService.get(parameter.getFeedId());
-		User user = parameter.getUser();
+    private final FcmMessageProvider fcmMessageProvider;
 
-		Reply reply = Reply.createReply(user, feed, parameter.getContent());
-		Reply saveReply = replyModuleService.save(reply);
+    @Transactional
+    public ReplyCreateResp createReply(ReplyCreateParam parameter) {
+        Feed feed = feedModuleService.get(parameter.getFeedId());
+        User user = parameter.getUser();
+        Reply reply = Reply.createReply(user, feed, parameter.getContent());
 
-		if (feed.getUser().getNotificationFlag() == Flag.Y) {
-			Notification notification = Notification.createNotification(feed.getUser(), NotificationType.REPLY,
-				saveReply.getId());
-			notificationModuleService.save(notification);
+        // 자식 댓글인 경우 부모 update
+        if (parameter.getParentId() != null) {
+            Reply parent = replyModuleService.getNormal(parameter.getParentId());
 
-			fcmMessageProvider.sendReplyMessage(feed.getUser().getEmail(), user.getEmail());
-		}
-		return new ReplyCreateResp(saveReply);
-	}
+            // 같은 게시글에 대한 댓글인지 체크
+            if (!feed.equals(parent.getFeed())) {
+                throw new Exception400("parentId", "부모 댓글과 게시물이 일치하지 않습니다.");
+            }
 
-	@Transactional
-	public void deleteReply(User user, Long replyId) {
-		Reply reply = replyModuleService.findByIdAndUserIdAndStatus(replyId, user.getId());
+            reply.updateParent(parent);
+        }
 
-		reply.deleteReplyByUser();
-	}
+        Reply saveReply = replyModuleService.save(reply);
 
-	@Transactional(readOnly = true)
-	public ReplyCreateResp.ListDTO getReplys(Long feedId, Long lastReplyId, Pageable pageable) {
-		Feed feed = feedModuleService.get(feedId);
+        if (feed.getUser().getNotificationFlag() == Flag.Y) {
+            Notification notification = Notification.createNotification(feed.getUser(),
+                NotificationType.REPLY,
+                saveReply.getId());
+            notificationModuleService.save(notification);
 
-		List<Reply> replyList = replyModuleService.getFeedReplys(feedId, lastReplyId, pageable);
+            fcmMessageProvider.sendReplyMessage(feed.getUser().getEmail(), user.getEmail());
+        }
+        return new ReplyCreateResp(saveReply);
+    }
 
-		List<ReplyCreateResp.ReplyDTO> replyListDTO = replyList.stream()
-			.map(ReplyCreateResp.ReplyDTO::new)
-			.collect(Collectors.toList());
+    @Transactional
+    public void deleteReply(User user, Long replyId) {
+        Reply reply = replyModuleService.getUserReply(replyId, user.getId());
 
-		return new ReplyCreateResp.ListDTO(feed, replyListDTO);
-	}
+        reply.deleteReplyByUser();
+    }
 
-	@Transactional
-	public void reportReply(ReportReplyParam parameter) {
-		Reply reply = replyModuleService.findByIdAndStatus(parameter.getReplyId());
+    @Transactional(readOnly = true)
+    public ReplyCreateResp.ListDTO getReplys(Long feedId, Long lastReplyId, Pageable pageable) {
+        Feed feed = feedModuleService.get(feedId);
 
-		User reported = reply.getUser();
-		User user = parameter.getUser();
+        List<Reply> replyList = replyModuleService.getFeedReplyPage(feedId, lastReplyId, pageable);
 
-		if (user.getId().equals(reported.getId())) {
-			throw new Exception404("자신의 댓글은 신고 할 수 없습니다.");
-		}
+        List<ReplyCreateResp.ReplyDTO> replyListDTO = replyList.stream()
+            .map(ReplyCreateResp.ReplyDTO::new)
+            .collect(Collectors.toList());
 
-		reportModuleService.hasReportedByType(user, ReportType.REPLY, reply.getId());
+        return new ReplyCreateResp.ListDTO(feed, replyListDTO);
+    }
 
-		Report report = Report.createReport(user, reported, ReportType.REPLY, reply.getId(),
-			parameter.getReportReason());
-		reportModuleService.save(report);
-	}
+    @Transactional
+    public void reportReply(ReportReplyParam parameter) {
+        Reply reply = replyModuleService.getNormal(parameter.getReplyId());
+
+        User reported = reply.getUser();
+        User user = parameter.getUser();
+
+        if (user.getId().equals(reported.getId())) {
+            throw new Exception404("자신의 댓글은 신고 할 수 없습니다.");
+        }
+
+        reportModuleService.hasReportedByType(user, ReportType.REPLY, reply.getId());
+
+        Report report = Report.createReport(user, reported, ReportType.REPLY, reply.getId(),
+            parameter.getReportReason());
+        reportModuleService.save(report);
+    }
 
 }
